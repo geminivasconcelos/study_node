f7a307f17acae5ee43052c0ae8df2d1a8cf7ebfe
Ge Vasconcelos <geminvasconceelos@gmail.com>
2022-02-21 20:20:16 -0300

    Concluído - Capturando links

--- criandoBiblioteca/index.js
+++ criandoBiblioteca/index.js
@@ -1,6 +1,20 @@
 const chalk = require('chalk');
 const fs = require('fs');
 
+
+
+function extraiLinks(texto){
+  const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm;
+  const arrayResultados = [];
+  let temp;
+  while(( temp = regex.exec(texto)) !== null){
+    arrayResultados.push({[temp[1]]: temp[2]})
+  }
+
+  
+  return arrayResultados;
+}
+
 function trataErro(erro) {
   throw new Error(chalk.red(erro.code, 'não há arquivo no caminho'));
 }
@@ -9,7 +23,7 @@ async function pegaArquivo(caminhoDoArquivo) {
   const encoding = 'utf-8';
   try {
     const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
-    console.log(chalk.green(texto))
+    console.log(extraiLinks(texto));
   } catch(erro) {
     trataErro(erro);
   }
@@ -34,3 +48,4 @@ async function pegaArquivo(caminhoDoArquivo) {
 // }
 
 pegaArquivo('./arquivos/texto1.md');
+
