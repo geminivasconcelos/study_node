8139596221fbcb9ef0f75eda9dc8f218311421a6
Ge Vasconcelos <geminvasconceelos@gmail.com>
2022-02-21 22:16:25 -0300

    Conluído: validando links - criando uma biblioteca

--- criandoBiblioteca/cli.js
+++ criandoBiblioteca/cli.js
@@ -1,15 +1,16 @@
 const chalk = require('chalk');
-
 const pegaArquivo = require('./index');
+const validaURLs = require('./http-validacao');
 
 const caminho = process.argv;
 
-async function processaTexto(caminhoDoArquivo){
-    const resultado = await pegaArquivo(caminhoDoArquivo[2]);
-    console.log(chalk.red('lista de links: '), resultado)
+async function processaTexto(caminhoDeArquivo) {
+  const resultado = await pegaArquivo(caminhoDeArquivo[2]);
+  if (caminho[3] === 'validar') {
+    console.log(chalk.yellow('links validados'), await validaURLs(resultado));
+  } else {
+    console.log(chalk.yellow('lista de links'), resultado);
+  }
 }
 
 processaTexto(caminho);
-
-
-// console.log(pegaArquivo(caminho[2]))
